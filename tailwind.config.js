c0edd33064a535711dbe83d089dc0f6330e3d6d5
ARiel Fernández <jarif78@gmail.com>
2020-07-31 19:30:57 -0300

    Ajustes varios

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,6 @@ module.exports = {
       'md': '768px',
       'lg': '1024px',
       'xl': '1280px',
-      '2xl': '1440px',
-      '3xl': '2000px',
     },    
     fontFamily: {
       'display': ['Manrope', 'sans-serif'],
